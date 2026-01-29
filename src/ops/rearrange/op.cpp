29bd22f516efb53eb72cb6ed612c0285a3c6bdc4
usersforsomebody <liheshishenxian@gmail.com>
2026-01-29 22:42:26 +0800

    Finish all the Task 1 Task 2

--- src/ops/rearrange/op.cpp
+++ src/ops/rearrange/op.cpp
@@ -2,10 +2,38 @@
 
 namespace llaisys::ops {
 template<typename T>
-void rearrange_cpu_kernal(tensor_t out, tensor_t in){
-    
+void rearrange_kernel(tensor_t out, tensor_t in){
+    const T*in_ptr=reinterpret_cast<T*>(in->data());
+    T*out_ptr=reinterpret_cast<T*>(out->data());
+    size_t total_num=in->numel();
+    size_t dim=in->ndim();
+    for(size_t i=0;i<total_num;i++){
+        size_t index_acc=i;
+        size_t in_offset=0;
+        size_t out_offset=0;
+        for(int j=dim-1;j>=0;j--){
+            size_t cur_shape=in->shape()[j];
+            size_t cur_index=index_acc%cur_shape;
+            index_acc/=cur_shape;
+            in_offset+=cur_index*in->strides()[j];
+            out_offset+=cur_index*out->strides()[j];
+        }
+        out_ptr[out_offset]=in_ptr[in_offset];
+    }  
 }
 void rearrange(tensor_t out, tensor_t in) {
-   
+    switch (in->dtype()) {
+        case LLAISYS_DTYPE_F16:
+            rearrange_kernel<llaisys::fp16_t>(out,in);
+            break;
+        case LLAISYS_DTYPE_BF16:
+            rearrange_kernel<llaisys::bf16_t>(out,in);
+            break;
+        case LLAISYS_DTYPE_F32:
+            rearrange_kernel<float>(out,in);
+            break;
+        default:
+            throw std::runtime_error("Not support this dtype!");
+    }
 }
 } // namespace llaisys::ops
--- src/ops/swiglu/op.cpp
+++ src/ops/swiglu/op.cpp
@@ -1,7 +1,37 @@
 #include "op.hpp"
+#include <cmath>
 
 namespace llaisys::ops {
+template<typename T>
+void swiglu_kernel(tensor_t out, tensor_t gate, tensor_t up){
+    T*out_ptr=reinterpret_cast<T*>(out->data());
+    const T*gate_ptr=reinterpret_cast<T*>(gate->data());
+    const T*up_ptr=reinterpret_cast<T*>(up->data());
+    size_t seqlen=out->shape()[0];
+    size_t intermediate_size=out->shape()[1];
+    for(size_t i=0;i<seqlen;i++){
+        for(size_t j=0;j<intermediate_size;j++){
+            float up_val=utils::cast<float>(up_ptr[i*intermediate_size+j]);
+            float gate_val=utils::cast<float>(gate_ptr[i*intermediate_size+j]);
+            float t_val=gate_val/(1+std::exp(-gate_val));
+            float out_val=up_val*t_val;
+            out_ptr[i*intermediate_size+j]=utils::cast<T>(out_val);
+        }
+    }
+}
 void swiglu(tensor_t out, tensor_t gate, tensor_t up) {
-    TO_BE_IMPLEMENTED();
+    switch (out->dtype()) {
+        case LLAISYS_DTYPE_F16:
+            swiglu_kernel<llaisys::fp16_t>(out,gate,up);
+            break;
+        case LLAISYS_DTYPE_BF16:
+            swiglu_kernel<llaisys::bf16_t>(out,gate,up);
+            break;
+        case LLAISYS_DTYPE_F32:
+            swiglu_kernel<float>(out,gate,up);
+            break;
+        default:
+            throw std::runtime_error("Not support this dtype!");
+    }
 }
 } // namespace llaisys::ops
