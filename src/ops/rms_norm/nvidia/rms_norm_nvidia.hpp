19683efe83be910b47d8269abd3eb0eabf497496
鼠鼠 <rpvdsmvbb8@privaterelay.appleid.com>
2026-03-04 13:09:51 +0800

    Add files via upload

--- src/ops/rms_norm/nvidia/rms_norm_nvidia.hpp
+++ src/ops/rms_norm/nvidia/rms_norm_nvidia.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "../../../tensor/tensor.hpp"
+#include <cstddef> // for size_t
 
 namespace llaisys::ops::nvidia {
-void rms_norm(tensor_t out, tensor_t in, tensor_t weight, float eps);
+
+void rms_norm(std::byte *out, const std::byte *in, const std::byte *weight,
+              llaisysDataType_t dtype, size_t rows, size_t cols, float eps);
+
 } // namespace llaisys::ops::nvidia
