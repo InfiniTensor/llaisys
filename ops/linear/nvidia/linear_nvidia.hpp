5d2e273b65b0bd75d9071b49329007963fb51a08
鼠鼠 <rpvdsmvbb8@privaterelay.appleid.com>
2026-03-04 13:11:30 +0800

    Delete ops directory

--- ops/linear/nvidia/linear_nvidia.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "llaisys.h"
-#include <cstddef>
-
-namespace llaisys::ops::nvidia {
-    // m: batch_size, n: out_features, k: in_features
-    void linear(std::byte *out, const std::byte *in, const std::byte *weight, const std::byte *bias,
-                llaisysDataType_t type, size_t m, size_t n, size_t k);
-}
